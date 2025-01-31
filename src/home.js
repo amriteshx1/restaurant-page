fc1c03b2986a60a776b3f36d95610d7c011f9bf6
amriteshx1 <amriteshx18@gmail.com>
2025-01-31 19:23:47 +0530

    use JS alone to generate the contents of home-page

---
+++ src/home.js
@@ -0,0 +1,20 @@
+let homePage = document.getElementById("content");
+const cnt = document.createElement('div');
+cnt.id = 'container';
+
+const para = document.createElement('p');
+para.className = 'heading';
+para.textContent = 'Web-Restaurant';
+
+const subPara = document.createElement('p');
+subPara.className = 'details';
+subPara.textContent = 'Welcome to our prestigious restaurant where we serve our premium and authentic foods just at a click. Book your table now & Enjoy the best meal of your life!';
+
+const btn = document.createElement('button');
+btn.className = 'book';
+btn.textContent = 'Book Your Table';
+
+cnt.appendChild(para);
+cnt.appendChild(subPara);
+cnt.appendChild(btn);
+homePage.appendChild(cnt);
