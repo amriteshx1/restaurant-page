9a4da10c8bc4483cf9b7f02bcc708fe5c03ff02e
amriteshx1 <amriteshx18@gmail.com>
2025-02-01 10:12:45 +0530

    Deployment commit

---
+++ 8886e0f5bb5ef5ab9b3a.js
@@ -0,0 +1,27 @@
+import "./style.css";
+
+import { homePageMain } from "./home.js";
+import { menuPage } from "./menu.js";
+import { aboutPage } from "./about.js";
+
+function clearPage(){
+    const c = document.getElementById('content');
+    c.innerHTML= '';
+};
+
+document.getElementById('homeBtn').addEventListener("click", () => {
+    clearPage();
+    homePageMain();
+});
+
+document.getElementById('menuBtn').addEventListener("click", () => {
+    clearPage();
+    menuPage();
+});
+
+document.getElementById('aboutBtn').addEventListener("click", () =>{
+    clearPage();
+    aboutPage();
+})
+
+homePageMain();
